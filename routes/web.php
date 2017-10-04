e10e86c3cab152b9803c9c57bbe0b23d5e627240
wangwenli1991 <1527179450@qq.com>
2017-10-04 13:41:40 +0800

    template1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/biao',function () {
+	return view('template1.index');
+});
